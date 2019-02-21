feb3e7ca84ca17a5c888f31dd138ca209ffc6bdb
Wei Wang <wei.wang@ia.ca>
2019-02-21 12:45:12 -0800

    Use semantic ui react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Menu } from 'semantic-ui-react';
 import SamplePageNav from './components/SamplePageNav';
 import './css/App.css';
 
@@ -13,16 +14,14 @@ class App extends Component {
     return (
       <Router>
         <div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/samples">Samples</Link>
-            </li>
-          </ul>
-
-          <hr />
+          <Menu>
+            <Menu.Item as={Link} to="/">
+              Home
+            </Menu.Item>
+            <Menu.Item as={Link} to="/samples">
+              Samples
+            </Menu.Item>
+          </Menu>
 
           <Route exact path="/" component={Home} />
           <Route path="/samples" component={SamplePageNav} />
