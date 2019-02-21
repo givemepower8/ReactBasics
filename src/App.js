ab084ba2352e81048b860ce096beef485134e6b4
Wei Wang <givemepower8@gmail.com>
2019-02-20 21:43:37 -0800

    add router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
-import logo from './img/logo.svg';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import SamplePageNav from './SamplePageNav';
 import './css/App.css';
 
+const Home = () => (
+  <div>
+      <h2>Home</h2>
+  </div>
+);
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/samples">samples</Link>
+                </li>                        
+            </ul>
+
+            <hr />
+
+            <Route exact path="/" component={Home} />
+            <Route path="/samples" component={SamplePageNav} />                   
+        </div>
+      </Router>
     );
   }
 }
